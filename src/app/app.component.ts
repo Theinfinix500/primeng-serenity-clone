1d3cbff307355352158edb2e682720bd20354bba
Theinfinix500 <jbaraali18@gmail.com>
2020-05-02 03:21:18 +0100

    made the first look of the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ViewChild,
+  ElementRef,
+  OnInit,
+  AfterViewInit,
+} from '@angular/core';
+import { fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'gestion';
+export class AppComponent implements OnInit, AfterViewInit {
+  @ViewChild('sidenav', { static: false }) sideNav: ElementRef;
+  @ViewChild('layoutWrapper', { static: false }) layoutWrapper: ElementRef;
+
+  ngOnInit() {}
+
+  ngAfterViewInit() {
+    fromEvent(this.sideNav.nativeElement, 'mouseover').subscribe(() => {
+      this.layoutWrapper.nativeElement.classList.toggle(
+        'layout-wrapper-active'
+      );
+    });
+    fromEvent(this.sideNav.nativeElement, 'mouseout').subscribe(() => {
+      this.layoutWrapper.nativeElement.classList.toggle(
+        'layout-wrapper-active'
+      );
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { InputTextModule } from 'primeng/inputtext';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, InputTextModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
